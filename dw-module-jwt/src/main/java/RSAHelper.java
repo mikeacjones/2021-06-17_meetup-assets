6ef76afd0fecdaf2d7a57023f3146a4651c724ae
Michael Jones <mjones@kotrs.com>
2021-06-03 10:04:30 -0400

    Added debugging for RSA, updated HMAC unit test output, and expanded signing algorithm choices for RSA to include 384 and 512.

--- dw-module-jwt/src/main/java/RSAHelper.java
+++ dw-module-jwt/src/main/java/RSAHelper.java
@@ -5,12 +5,12 @@ import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Base64;
 
 public class RSAHelper {
-    public static String signString(String content, String privateKeyContent) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, UnsupportedEncodingException, SignatureException {
+    public static String signString(String content, String privateKeyContent, String algorithm) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, UnsupportedEncodingException, SignatureException {
         KeyFactory kf = KeyFactory.getInstance("RSA");
         PKCS8EncodedKeySpec keySpecPKCS8 = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKeyContent));
         PrivateKey pk = kf.generatePrivate(keySpecPKCS8);
 
-        Signature privateSignature = Signature.getInstance("SHA256withRSA");
+        Signature privateSignature = Signature.getInstance(algorithm);
         privateSignature.initSign(pk);
         privateSignature.update(content.getBytes("UTF-8"));
         byte[] s = privateSignature.sign();
